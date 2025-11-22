9e171fb18d897969afdcd4d97354c3c3689aa3c1
Nikash M.S. <107871733+msnikash@users.noreply.github.com>
2025-11-22 19:23:13 +0530

    Updated bed.sql
    Added a loop to add more free beds

--- bed.sql
+++ bed.sql
@@ -1,6 +1,5 @@
--- Create Bed database
+-- Create BED database
 CREATE DATABASE IF NOT EXISTS bed;
-
 USE bed;
 
 -- Create patients table
@@ -11,23 +10,38 @@ CREATE TABLE IF NOT EXISTS patients (
     condition_priority VARCHAR(10),
     bed_allocated BOOLEAN DEFAULT FALSE,
     allocated_bed_id INT NULL,
-    allocated_days INT NULL
+    allocated_days INT NULL,
+    start_date DATE NULL,
+    end_date DATE NULL,
+    allocated_on DATE NULL,
+    discharged BOOLEAN DEFAULT FALSE
 );
 
 -- Create beds table
 CREATE TABLE IF NOT EXISTS beds (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    bed_number VARCHAR(10),
+    bed_number VARCHAR(20),
     is_occupied BOOLEAN DEFAULT FALSE
 );
 
--- Insert BED beds
-INSERT INTO beds (bed_number, is_occupied) VALUES
-('BED-101', FALSE),
-('BED-102', FALSE),
-('BED-103', FALSE),
-('BED-104', FALSE);
+-- Insert 100 beds
+DELIMITER $$
 
--- Optional: check tables
+CREATE PROCEDURE insert_beds()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    WHILE i <= 100 DO
+        INSERT INTO beds (bed_number, is_occupied)
+        VALUES (CONCAT('BED-', LPAD(i, 3, '0')), FALSE);
+        SET i = i + 1;
+    END WHILE;
+END$$
+
+DELIMITER ;
+
+CALL insert_beds();
+
+
+-- Optional checks
 SELECT * FROM beds;
 SELECT * FROM patients;
